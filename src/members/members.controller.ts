8548ca9c8cd8082cc437c46daa82a5345b5fa63e
daewoo <bdkfasd@naver.com>
2022-12-20 09:54:00 +0900

    .

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -8,27 +8,27 @@ import {
   Patch,
   Body,
   Query,
-} from '@nestjs/common';
-import { MembersService } from './members.service';
-import { Member } from './entities/member.entity';
-import { CreateMemberDto } from './dto/create-member.dto';
-import { UpdateMemberDto } from './dto/update-member.dto';
-import { MembersPage } from './entities/members-page.entity';
+} from "@nestjs/common";
+import { MembersService } from "./members.service";
+import { Member } from "./entities/member.entity";
+import { CreateMemberDto } from "./dto/create-member.dto";
+import { UpdateMemberDto } from "./dto/update-member.dto";
+import { MembersPage } from "./entities/members-page.entity";
 
-@Controller('members')
+@Controller("members")
 export class MembersController {
   constructor(private readonly membersService: MembersService) {}
 
   @Get()
   getMemberListWithQuery(
-    @Query('page') page?: number,
-    @Query('per_page') per_page?: number,
+    @Query("page") page?: number,
+    @Query("per_page") per_page?: number
   ): Promise<MembersPage> {
-    return this.membersService.getMemberListWithQuery(page || 1, per_page || 5);
+    return this.membersService.getMemberListWithQuery(page || 1, per_page || 6);
   }
 
-  @Get(':id')
-  getMemberById(@Param('id') memberId: number): Promise<Member> {
+  @Get(":id")
+  getMemberById(@Param("id") memberId: number): Promise<Member> {
     return this.membersService.getMemberById(memberId);
   }
 
@@ -37,23 +37,23 @@ export class MembersController {
     return this.membersService.createMember(memberData);
   }
 
-  @Delete(':id')
-  deleteMemberById(@Param('id') memberId: number) {
+  @Delete(":id")
+  deleteMemberById(@Param("id") memberId: number) {
     return this.membersService.deleteMemberById(memberId);
   }
 
-  @Put(':id')
+  @Put(":id")
   putMember(
-    @Param('id') memberId: number,
-    @Body() updateData: UpdateMemberDto,
+    @Param("id") memberId: number,
+    @Body() updateData: UpdateMemberDto
   ) {
     return this.membersService.updateMember(memberId, updateData);
   }
 
-  @Patch(':id')
+  @Patch(":id")
   patchMember(
-    @Param('id') memberId: number,
-    @Body() updateData: UpdateMemberDto,
+    @Param("id") memberId: number,
+    @Body() updateData: UpdateMemberDto
   ) {
     return this.membersService.updateMember(memberId, updateData);
   }
