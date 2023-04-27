36742a41e4e164c3c38c77be2156e2198781a66e
Daewoo Koh <bdkfasd@naver.com>
2023-04-27 10:17:23 +0900

    test2

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -5,14 +5,14 @@ import {
   Post,
   Delete,
   Patch,
-  Body,
-} from '@nestjs/common';
-import { MoviesService } from './movies.service';
-import { Movie } from './entities/movie.entity';
-import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update-movie.dto';
+  Body
+} from "@nestjs/common";
+import { MoviesService } from "./movies.service";
+import { Movie } from "./entities/movie.entity";
+import { CreateMovieDto } from "./dto/create-movie.dto";
+import { UpdateMovieDto } from "./dto/update-movie.dto";
 
-@Controller('movies')
+@Controller("movies")
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
@@ -21,8 +21,8 @@ export class MoviesController {
     return this.moviesService.getAll();
   }
 
-  @Get(':id')
-  getOne(@Param('id') movieId: number): Movie {
+  @Get(":id")
+  getOne(@Param("id") movieId: number): Movie {
     return this.moviesService.getOne(movieId);
   }
 
@@ -31,13 +31,13 @@ export class MoviesController {
     return this.moviesService.create(movieData);
   }
 
-  @Delete(':id')
-  remove(@Param('id') movieId: number) {
+  @Delete(":id")
+  remove(@Param("id") movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
-  @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+  @Patch(":id")
+  patch(@Param("id") movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 }
